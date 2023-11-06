1eeda1d7ef594ef31db0add29cc2e223e8111d9e
a.varenyshev <alex.varenyshev@gmail.com>
2023-11-06 19:33:58 +0300

    finished j5 checking

--- src/test/java/searchTestGitHub.java
+++ src/test/java/searchTestGitHub.java
@@ -1,21 +1,48 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Href;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class searchTestGitHub {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
 //Открыть GitGub
         open("https://github.com/");
 // Выполнить поиск по 'Selenide'
-        $("[placeholder='Search or jump to...']").setValue("selenide").pressEnter();
-// Открыть страницу 'Selenide'
+        $(byText("Search or jump to...")).click();
+        $(byName("query-builder-test")).setValue("selenide").pressEnter();
+        // Открыть страницу 'Selenide'
+        $(byXpath("//a[@href='/selenide/selenide']")).click();
 //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
 //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-// Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        // Откройте страницу SoftAssertions
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+//проверьте что внутри есть пример кода для JUnit5
+        $("#user-content-3-using-junit5-extend-test-class").sibling(0).shouldHave(exactText("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
     }
+
 }
