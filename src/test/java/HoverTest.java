80cf6925b9a86b3308b7b362bbc61a344990ab34
aa.varenyshev <aa.varenyshev@tensor.ru>
2023-11-07 14:15:18 +0300

    Add new class

---
+++ src/test/java/HoverTest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+public class HoverTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void testHoverGithub(){
+    //Открыть GitGub
+    open("https://github.com/");
+    }
+
+
+}
