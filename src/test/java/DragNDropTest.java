9195b5c6607a053ef2c5fff89a8861edd5286735
aa.varenyshev <aa.varenyshev@tensor.ru>
2023-11-10 16:24:06 +0300

    added better check

--- src/test/java/DragNDropTest.java
+++ src/test/java/DragNDropTest.java
@@ -1,21 +1,26 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragNDropTest {
     @BeforeAll
     static void beforeAll() {
-        Configuration.holdBrowserOpen = true;}
+        Configuration.holdBrowserOpen = true;
+    }
 
-@Test
-    void dragNDroptest(){
+    @Test
+    void dragNDroptest() {
         //открываем https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
-    SelenideElement selenideElement = $(element("#column-a")).dragAndDropTo("#column-b");
-    $("#column-a").shouldHave(Condition.value("<header>B</header>"));
-}
+        //Перетаскиваем А на Б
+        SelenideElement selenideElement = $(element("#column-a")).dragAndDropTo("#column-b");
+        //Проверяем результат
+        $("#column-a").shouldHave(exactText("B"));
+        $("#column-b").shouldHave(exactText("A"));
+
+    }
 
 }
